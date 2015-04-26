de8f2328f79a94e2e74fa564a491c1716cf1ce63
Karl <kturrell@hotmail.com>
2015-04-26 17:17:50 +0100

    Started building models and views (squashme)

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,17 @@
-// Some backbone code here
+(function($){
+   var Task = Backbone.Model.extend({});
+   task = new Task({title: 'RandomTask'});
+
+   var TaskView = Backbone.View.extend({
+      el: $('#taskspace'),
+      initialize: function(){
+         _.bindAll(this, 'render');
+         this.render();
+      },
+      render: function(){
+         $(this.el).html('<li>' + this.model.get('title') + '</li>');
+      }
+   });
+
+   var taskView = new TaskView({model: task});
+})(jQuery);
