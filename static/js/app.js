526a20f85993f00c70c732f7ad0cfacfc02514e6
Karl <kturrell@hotmail.com>
2015-05-24 21:50:46 +0100

    Updated libraries and finished inc/reset functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,36 @@
 (function(){
-   var app = angular.module('tasks', []);
+   var app = angular.module('tasks', ['ui.bootstrap']);
 
-   app.controller('TaskController', ['$http', function($http){
+   app.controller('TaskController', ['$http', '$scope', function($http, $scope){
       var tc = this;
       tc.tasks = [];
+
+      $scope.getType = function(progress, max){
+         var value = progress / max;
+         if(value < 0.25){
+            return 'danger';
+         } else if(value < 0.75){
+            return 'warning';
+         } else {
+            return 'success';
+         }
+      };
+
+      $scope.addTask = function(){
+         alert("Got here!");
+      };
+
+      $scope.incTask = function(task){
+         if(task.progress < task.weeklyfreq){
+            task.progress += 1;
+         }
+         $http.get('/incProgress/?taskname='+task.name);
+      };
+
+      $scope.resetTask = function(task){
+         task.progress = 0;
+         $http.get('/resetProgress/?taskname='+task.name);
+      }
 
       $http.get('/api/v1/task/?format=json').success(function(data){
          tc.tasks = data.objects;
