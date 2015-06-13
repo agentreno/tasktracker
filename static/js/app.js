9b080bcb7a1595d028f419d1fefb791ea81d7a1c
Karl <kturrell@hotmail.com>
2015-06-13 15:02:54 +0100

    Revert to Django 1.7 in virtualenv
    Tastypie compatibility issues with Django 1.8, created a virtual environment
    as a temporary measure. Started using ngRoute and splitting up into separate
    templates.

--- static/js/app.js
+++ static/js/app.js
@@ -1,57 +1,12 @@
 (function(){
-   var app = angular.module('tasks', ['ui.bootstrap']);
-
-   app.controller('TaskController', ['$http', '$scope', function($http, $scope){
-      var tc = this;
-      this.tasklist = [];
-
-      this.getType = function(progress, max){
-         var value = progress / max;
-         if(value < 0.25){
-            return 'danger';
-         } else if(value < 0.75){
-            return 'warning';
-         } else {
-            return 'success';
-         }
-      };
-
-      this.addTask = function(task){
-         $http.get('/addTask?taskname='+task.name+
-               '&taskdesc='+task.desc+
-               '&taskfreq='+task.freq);
-         tc.tasklist.push({
-            "name":task.name,
-            "description":task.desc,
-            "weeklyfreq":task.freq,
-            "progress":0
-         });
-         task.name = "";
-         task.desc = "";
-         task.freq = 0;
-      };
-
-      this.incTask = function(task){
-         if(task.progress < task.weeklyfreq){
-            task.progress += 1;
-         }
-         $http.get('/incProgress/?taskname='+task.name);
-      };
-
-      this.resetTask = function(task){
-         task.progress = 0;
-         $http.get('/resetProgress/?taskname='+task.name);
-      };
-
-      this.deleteTask = function(task){
-         tc.tasklist = tc.tasklist.filter(function(oldtask){
-            return oldtask.name != task.name;
-         });
-         $http.get('/deleteTask/?taskname='+task.name);
-      };
-
-      $http.get('/api/v1/task/?format=json').success(function(data){
-         tc.tasklist = data.objects;
-      });
-   }]);
+   var app = angular.module('tasks', ['ngRoute', 'controllers', 'ui.bootstrap']);
+   app.config(['$routeProvider',
+      function($routeProvider) {
+         $routeProvider.
+            when('/tasks', {
+               templateUrl: 'tasks.html',
+               controller: 'TaskController'
+            });
+      }
+   ]);
 })();
