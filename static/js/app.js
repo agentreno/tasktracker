ca37f43ef59f491b94ac307a62a11b90c4908073
Karl <kturrell@hotmail.com>
2015-06-14 11:44:14 +0100

    Tidier transitions and states using ui-router/gsapify

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,39 @@
 (function(){
    var app = angular.module('tasks', 
-         ['ngRoute', 'ngAnimate', 'controllers', 'ui.bootstrap']);
-   app.config(['$routeProvider',
-      function($routeProvider) {
-         $routeProvider.
-            when('/tasks', {
+         ['ui.router', 'controllers', 'ui.bootstrap', 'hj.gsapifyRouter']);
+   app.config(
+      function($stateProvider, gsapifyRouterProvider) {
+         gsapifyRouterProvider.defaults = {
+            enter: 'fadeIn',
+            leave: 'fadeOut'
+         };
+
+         gsapifyRouterProvider.initialTransitionEnabled = true;
+
+         gsapifyRouterProvider.transition('fadeIn', {
+            duration: 0.5,
+            delay: 1,
+            css: {
+               opacity: 1
+            }
+         });
+         gsapifyRouterProvider.transition('fadeOut', {
+            duration: 0.5,
+            css: {
+               opacity: 0,
+            }
+         });
+         $stateProvider
+            .state('dashboard', {
+               url: '/',
                templateUrl: 'partials/dashboard.html',
                controller: 'TaskController'
             })
-            .when('/stats', {
+            .state('stats', {
+               url: '/',
                templateUrl: 'partials/stats.html',
                controller: 'TaskController'
-            })
-            .otherwise({
-               redirectTo: '/tasks'
             });
       }
-   ]);
+   );
 })();
