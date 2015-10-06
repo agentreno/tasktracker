a63f375c2666fc38df5c924fed1cb03c504b599a
Karl <kturrell@hotmail.com>
2015-10-06 21:18:04 +0100

    Production change to allow detection of Heroku database

--- tracker/settings.py
+++ tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -64,6 +64,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+if dj_database_url.config() != {}:
+   DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
